b0d9ace6e8ea4c96bfe926008e93ba06711cb3ff
Marino <107618043+marin-o@users.noreply.github.com>
2024-08-21 16:52:18 +0200

    Init projects and models

---
+++ OnlineLibrary.Domain/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineLibrary.Domain
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
