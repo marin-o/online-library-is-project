a0213ae5dac8f3c20b6041812ab07f3681f7a4e9
Marino <107618043+marin-o@users.noreply.github.com>
2024-08-23 14:40:53 +0200

    cart dto

---
+++ OnlineLibrary.Domain/DTO/BorrowingCartDTO.cs
@@ -0,0 +1,14 @@
+﻿using OnlineLibrary.Domain.Models.RelationalModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLibrary.Domain.DTO
+{
+    public class BorrowingCartDTO
+    {
+        List<BookInBorrowingCart>? BooksInCart { get; set; }
+    }
+}
