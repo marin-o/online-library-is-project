ce80037f677aee2410da379b8e3aa3868c655fd3
Marino <107618043+marin-o@users.noreply.github.com>
2024-08-23 13:58:04 +0200

    models fully working

---
+++ OnlineLibrary.Repository/Migrations/20240823114246_updated-book-model.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineLibrary.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedbookmodel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Available",
+                table: "Books",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Available",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
