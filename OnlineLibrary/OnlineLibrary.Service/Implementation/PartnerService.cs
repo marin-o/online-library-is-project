1d9887c1b512f38750902abd5cbdac4e6ff82245
Marino <107618043+marin-o@users.noreply.github.com>
2024-09-15 16:13:30 +0200

    ETL *really* completed

--- OnlineLibrary/OnlineLibrary.Service/Implementation/PartnerService.cs
+++ OnlineLibrary/OnlineLibrary.Service/Implementation/PartnerService.cs
@@ -22,12 +22,14 @@ public class PartnerService : IPartnerService
 
             foreach (var book in books)
             {
+                var formattedISBN = FormatISBN(book.ISBN);
+
                 var kniga = new Book
                 {
                     Id = book.Id,
-                    Title = book.Title,
+                    Title = $"{book.Title} <i>- From Partners</i>",
                     ImageUrl = book.CoverImage,
-                    ISBN = book.ISBN,
+                    ISBN = formattedISBN,
                     Quantity = 0
                 };
                 localBooks.Add(kniga);
@@ -35,5 +37,14 @@ public class PartnerService : IPartnerService
 
             return localBooks;
         }
+    }
+
+    private string FormatISBN(string isbn)
+    {
+        if (isbn.Length != 10)
+        {
+            return isbn + " - could not format.";
+        }
+        return $"{isbn.Substring(0, 1)}-{isbn.Substring(1, 4)}-{isbn.Substring(5, 4)}-{isbn.Substring(9, 1)}";
     }
 }
